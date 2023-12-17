3bb9db7b499c88ad04ec4b967e67e6a086b32986
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-16 22:08:37 -0600

    Finished the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,20 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
+import aboutHero from "../assets/images/about-hero.png"
 
 export default function About(){
     return(
-        <div>
-            This is the About Section
+        <div className="about-page-container">
+            <img className="about-hero-image" src={aboutHero}></img>
+            <div className="about-page-content">
+                <h1>Don&apos;t squeeze in a sedan when you can relax in a van.</h1>
+                <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
+                <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+            </div>
+            <div className="about-page-cta">
+                <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                <Link className="link-button" to="../vans">Explore our vans</Link>
+            </div>
         </div>
     )
 }
