2bae09bc8d05f1aff90367f0d455876b5bf9d9fd
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-16 21:55:50 -0600

    Completed home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { Link, NavLink } from "react-router-dom"
 export default function Header(){
     return(
         <header>
-            <Link to="/" className="site-logo">#vanlife</Link>
+            <Link to="." className="site-logo">#vanlife</Link>
             <nav>
-                <NavLink to="/host">Host</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/vans">Vans</NavLink>
+                <NavLink to="host">Host</NavLink>
+                <NavLink to="about">About</NavLink>
+                <NavLink to="vans">Vans</NavLink>
             </nav>
         </header>
     )
