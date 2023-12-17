dc40766956e41aafd46345cf1477850ad16492eb
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-16 20:51:12 -0600

    created a general layout of the app with header, main and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./server"
+import Layout from "../src/components/Layout.jsx"
 
 export default function App(){
     return(
         <div>
-            App
+            <Layout />
         </div>
     )
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+export default function Layout(){
+    return(
+        <div>
+            <Header />
+            <main>
+                Main Here
+            </main>
+            <Footer />
+        </div>
+    )
+}
