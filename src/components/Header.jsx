0a377c2c348b98354718e58ab68ad18771e4b728
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-16 22:16:35 -0600

    added isActive feature such that active links in header are underlined

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header(){
+    const activeStyles = {
+        textDecoration : "underline",
+        fontWeight : "bold",
+        color : "#161616"
+    }
+
     return(
         <header>
             <Link to="." className="site-logo">#vanlife</Link>
             <nav>
-                <NavLink to="host">Host</NavLink>
-                <NavLink to="about">About</NavLink>
-                <NavLink to="vans">Vans</NavLink>
+                <NavLink to="host" style={({isActive}) => isActive ? activeStyles : null}>Host</NavLink>
+                <NavLink to="about" style={({isActive}) => isActive ? activeStyles : null}>About</NavLink>
+                <NavLink to="vans" style={({isActive}) => isActive ? activeStyles : null}>Vans</NavLink>
             </nav>
         </header>
     )
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,8 +2,11 @@ import React from "react"
 
 export default function Vans(){
     return(
-        <div>
-            This is the Vans Section
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                Van List here from api
+            </div>
         </div>
     )
 }
