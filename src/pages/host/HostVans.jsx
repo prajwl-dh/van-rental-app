7e2cbfbeb97058ea957a13d26cdd4d1a84287ec2
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-18 11:59:26 -0600

    added state for host vans and function to call the api and save the result to a state

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
 export default function HostVans(){
+    const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        async function fetchHostVans(){
+            const response = await fetch('/api/host/vans')
+            const data = await response.json()
+            setVans((prev) => {
+                return data.vans
+            })
+        }
+        fetchHostVans()
+    },[])
+    
     return(
         <div>
             <h1>Host Vans Goes Here</h1>
